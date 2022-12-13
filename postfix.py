4dde7d0dcbd09e2ccea692e5b39d8c254925e636
emapodhorna <ema.podhorna@gmail.com>
2022-12-13 13:11:34 +0100

    pridany to_infix

--- postfix.py
+++ postfix.py
@@ -7,16 +7,33 @@ def eval_expr(s,d={}):
     for item in arg:
         if item in ("+","-","*","/"):
             if item=="+":
-                zas.append(zas.pop()+zas.pop())
+                zas.append(zas.pop(-2)+zas.pop())
             elif item=="-":
-                zas.append(zas.pop(0)-zas.pop())
+                zas.append(zas.pop(-2)-zas.pop())
             elif item=="*":
-                zas.append(zas.pop()*zas.pop())
+                zas.append(zas.pop(-2)*zas.pop())
             elif item=="/":
-                zas.append(zas.pop(0)//zas.pop())
+                zas.append(zas.pop(-2)//zas.pop())
         elif item.isdigit():
             zas.append(int(item))
         else:
             zas.append(d[item])
 
+    return zas[0]
+
+def to_infix(s):
+    arg=s.split()
+    zas=[]
+
+    for item in arg:
+        if item=="+":
+            zas.append( "( " + zas.pop(-2) + " + " + zas.pop() + " )" )
+        elif item=="-":
+            zas.append( "( " + zas.pop(-2) + " - " + zas.pop() + " )" )
+        elif item=="*":
+            zas.append( "( " + zas.pop(-2) + " * " + zas.pop() + " )" )
+        elif item=="/":
+            zas.append( "( " + zas.pop(-2) + " / " + zas.pop() + " )" )
+        else:
+            zas.append(item)
     return zas[0]
